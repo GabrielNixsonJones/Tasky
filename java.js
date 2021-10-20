00d15d951dd9f4d13280ba77289359968da30ddd
GabrielNixsonJones <86356816+GabrielNixsonJones@users.noreply.github.com>
2021-10-20 19:44:04 +0530

    Add files via upload
    finally done

---
+++ java.js
@@ -0,0 +1,80 @@
+let globalStore = [];//array of objects
+const taskContainer = document.querySelector(".task__container")
+
+const generateNewCard = (taskData) => `
+  <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+  <div class="card card border border-secondary ">
+  <div class="card-header  border-secondary d-flex justify-content-end gap-2">
+   <button type="button" class="btn btn-outline-success" ><i class="fas fa-pencil-alt"></i></button>
+  <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
+  </div>
+  <div class="card-body">
+  <img class="card-img-top" src=${taskData.imageUrl} alt="...">
+   <h5 class="card-title mt-3 fw-bolder text-primary">${taskData.taskTitle}</h5>
+   <p class="card-text">${taskData.taskType}</p>
+   <a href="#" class="btn btn-primary">${taskData.taskDescription}</a>
+  </div>
+  </div>
+  </div>
+  `;
+
+
+//loadInitialCardData() function , it render html card using data in localStorage and prevent from disapper of cards
+const loadInitialCardData = () => {
+  //get data from localStorage
+  const getCardData = localStorage.getItem("data");
+
+  //converting it into normal javascript object
+  const {cards} = JSON.parse(getCardData);   //{cards} -> destructing
+
+  //loop over the array of objects from king , inject it into dom
+  cards.map((cardObject) => {
+       taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject)); // create dynamic HTML cards and insert it into DOM
+      globalStore.push(cardObject); // push card to Global Storage
+  })
+};
+
+
+// Delete Card
+const deleteCard = (event) => {
+    event = window.event;
+  const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+  globalStore =  globalStore.filter((cardObject) =>{
+      return cardObject.id !== targetID
+    });
+
+    localStorage.setItem("data", JSON.stringify({cards:globalStore} ));
+
+    if (tagname === "BUTTON") {
+        return  document.querySelector(".task__container").removeChild(event.target.parentNode.parentNode.parentNode);
+    } else {
+        return  document.querySelector(".task__container").removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    };
+};
+
+
+
+//savechanges() function
+const saveChanges = () =>{
+  //data from   modal and extract from html and stores in taskData object
+  const taskData = {
+    id:`${Date.now()}`,
+    imageUrl:document.getElementById("imageurl").value,
+    taskTitle:document.getElementById("tasktitle").value,
+    taskType:document.getElementById("tasktype").value,
+    taskDescription:document.getElementById("taskdescription").value
+
+  };
+
+
+
+
+   taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  globalStore.push(taskData);
+
+
+  localStorage.setItem("data", JSON.stringify({cards:globalStore} )); // Stores data in Key - Value pairs
+
+};
